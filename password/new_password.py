a3f874b56cad9c192c30918dbb0e17b3060a10e5
Родиоша <ribnikov.rodion@gmail.com>
2024-11-15 21:40:07 +0500

    Added a new tests and new scenario of the generate_password()

--- password/new_password.py
+++ password/new_password.py
@@ -5,9 +5,12 @@ def generate_password(length=12):
     """Генерация случайного пароля заданной длины."""
     characters = string.ascii_letters + string.digits + string.punctuation
     password = ''
-    for i in range(length):
-        password += random.choice(characters)
-    return password
+    if type(length) == int:
+        for i in range(length):
+            password += random.choice(characters)
+        return password
+    else:
+        return 'Неправильный ввод длины пароля'
 
 # Пример использования
 password_length = 12  # Вы можете выбрать любую длину пароля
